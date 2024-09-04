328405b790060e7ac56dbe0160d9a4f74b5cf758
LarsMadproduction <schumacher90lars@gmail.com>
2024-09-04 13:55:24 +0200

    add task revised

--- js/add_task.js
+++ js/add_task.js
@@ -1,34 +1,34 @@
 function changeColorUrgent() {
-  let urgentRef = document.getElementById("urgentSpan");
+  let urgentRef = document.getElementById("urgent_span");
   let urgentSvg1Ref = document.getElementById("urgentSVG1");
   let urgentSvg2Ref = document.getElementById("urgentSVG2");
   if (onclick = true) {
-    urgentRef.classList.remove("urgent_button");
-    urgentRef.classList.add("clicked_urgent");
+    urgentRef.classList.remove("urgent-button");
+    urgentRef.classList.add("clicked-urgent");
     urgentSvg1Ref.setAttribute("fill", "#ffffff");
     urgentSvg2Ref.setAttribute("fill", "#ffffff");
   }
 }
 
 function changeColorMedium() {
-  let mediumRef = document.getElementById("mediumSpan");
+  let mediumRef = document.getElementById("medium_span");
   let mediumSVG1Ref = document.getElementById("mediumSVG1");
   let mediumSVG2Ref = document.getElementById("mediumSVG2");
   if (onclick = true) {
-    mediumRef.classList.remove("medium_button");
-    mediumRef.classList.add("clicked_medium");
+    mediumRef.classList.remove("medium-button");
+    mediumRef.classList.add("clicked-medium");
     mediumSVG1Ref.setAttribute("fill", "white");
     mediumSVG2Ref.setAttribute("fill", "white");
   }
 }
 
 function changeColorLow() {
-  let lowRef = document.getElementById("lowSpan");
+  let lowRef = document.getElementById("low_span");
   let lowSVG1Ref = document.getElementById("lowSVG1");
   let lowSVG2Ref = document.getElementById("lowSVG2");
   if (onclick = true) {
-    lowRef.classList.remove("low_button");
-    lowRef.classList.add("clicked_low");
+    lowRef.classList.remove("low-button");
+    lowRef.classList.add("clicked-low");
     lowSVG1Ref.setAttribute("fill", "white");
     lowSVG2Ref.setAttribute("fill", "white");
   }
