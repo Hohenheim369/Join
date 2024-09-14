15c5eaf2417d66744c936f631ed0e203d0229dd0
Susanne Renken <renken.susanne@gmail.com>
2024-09-14 20:00:39 +0200

    finish submit

--- scripts/access.js
+++ scripts/access.js
@@ -61,11 +61,15 @@ function setupPasswordFieldInteractions(
   togglePassword.addEventListener("mouseleave", () =>
     hidePassword(passwordField, togglePassword)
   );
-  togglePassword.addEventListener("touchstart", () =>
-    showPassword(passwordField, togglePassword)
+  togglePassword.addEventListener(
+    "touchstart",
+    () => showPassword(passwordField, togglePassword),
+    { passive: true }
   );
-  togglePassword.addEventListener("touchend", () =>
-    hidePassword(passwordField, togglePassword)
+  togglePassword.addEventListener(
+    "touchend",
+    () => hidePassword(passwordField, togglePassword),
+    { passive: true }
   );
 }
 
@@ -82,8 +86,9 @@ function showPassword(passwordField, togglePassword) {
 
 function hidePassword(passwordField, togglePassword) {
   passwordField.type = "password";
-  togglePassword.src = "/assets/img/png/visibility_off.png";
+  togglePassword.src = "/assets/img/png/visibility_off.png";  
 }
+
 
 // Muss noch bearbeitet werden
 function loginAsGuest() {
@@ -92,14 +97,19 @@ function loginAsGuest() {
 
 // ------------------------------------------------------------------
 // ------------------------------------------------------------------
+// ------------------------------------------------------------------
+// ------------------------------------------------------------------
 
 const BASE_URL_S =
   "https://joinsusanne-default-rtdb.europe-west1.firebasedatabase.app/";
 
-// 1.  Form validieren-----------------------------------------------
-function removeNoticeButtonBg(){
-  const checkButton = document.getElementById("signup_check_off");    
-  checkButton.classList.remove('bg-alert');
+// ------------------------------------------------------------------
+// ------------------------------------------------------------------
+
+
+function removeNoticeButtonBg() {
+  const checkButton = document.getElementById("signup_check_off");
+  checkButton.classList.remove("bg-alert");
 }
 
 function isLegalAccepted() {
@@ -108,17 +118,64 @@ function isLegalAccepted() {
   return isChecked;
 }
 
+function validateLegalAcceptance(noticeField) {
+  const acceptedLegal = isLegalAccepted();
+  if (!acceptedLegal) {
+    const checkButton = document.getElementById("signup_check_off");
+    console.log("Please accept the Legal notice.");
+    noticeField.innerHTML += `<div>Please accept the Legal notice.</div>`;
+    checkButton.classList.add("bg-alert");
+    return false;
+  }
+  return true;
+}
+
 function validateEmail(email, noticeField) {
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  const isValidEmail = emailRegex.test(email);
-
-  if (!isValidEmail) {
+  if (!emailRegex.test(email)) {
     console.log("Invalid email format.");
     noticeField.innerHTML += `<div>Please enter a valid email address.</div>`;
-    document.getElementById("signup_email").classList.add('border-alert');
+    document.getElementById("signup_email").classList.add("border-alert");
+    return false;
+  }
+  return true;
+}
+
+function checkNameNotEmpty(name, noticeField) {
+  if (name.trim() === "") {
+    console.log("No name entered.");
+    noticeField.innerHTML += `<div>Please enter a name.</div>`;
+    return false;
+  }
+  return true;
+}
+
+function checkNameCharacters(name, noticeField) {
+  const nameRegex = /^[A-Za-z\s]+$/;
+
+  if (!nameRegex.test(name)) {
+    console.log("Name contains invalid characters.");
+    noticeField.innerHTML += `<div>Your name should only contain letters and spaces.</div>`;
+    return false;
+  }
+  return true;
+}
+
+function validateName(name, noticeField) {
+  let isValidName = true;
+  const nameField = document.getElementById("signup_name");
+
+  if (!checkNameNotEmpty(name, noticeField)) {
+    nameField.classList.add("border-alert");
+    isValidName = false;
   }
 
-  return isValidEmail;
+  if (!checkNameCharacters(name, noticeField)) {
+    nameField.classList.add("border-alert");
+    isValidName = false;
+  }
+
+  return isValidName;
 }
 
 function checkPasswordMatch(password, cPassword, noticeField) {
@@ -131,8 +188,9 @@ function checkPasswordMatch(password, cPassword, noticeField) {
 }
 
 function checkPasswordComplexity(password, noticeField) {
-  const complexityRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>/?]).{8,}$/;
-  
+  const complexityRegex =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>/?]).{8,}$/;
+
   if (!complexityRegex.test(password)) {
     console.log("Password does not meet complexity requirements.");
     noticeField.innerHTML += `<div>Your password must be at least 8 characters long and contain at least 1 uppercase letter, 1 lowercase letter, 1 number, and 1 special character.</div>`;
@@ -147,36 +205,31 @@ function validatePassword(password, cPassword, noticeField) {
   const cPasswordField = document.getElementById("signup_c_password");
 
   if (!checkPasswordComplexity(password, noticeField)) {
-    passwordField.classList.add('border-alert');
+    passwordField.classList.add("border-alert");
     isValidPassword = false;
   }
 
   if (!checkPasswordMatch(password, cPassword, noticeField)) {
-    passwordField.classList.add('border-alert');
-    cPasswordField.classList.add('border-alert');
+    passwordField.classList.add("border-alert");
+    cPasswordField.classList.add("border-alert");
     isValidPassword = false;
   }
   return isValidPassword;
 }
 
-function validateInputs(email, password, cPassword) {
+function validateInputs(email, name, password, cPassword) {
   const noticeField = document.getElementById("notice_field");
   noticeField.innerHTML = "";
 
-  let isValid = true;
-  let acceptedLegal = isLegalAccepted();
+  const validations = [
+    () => validateEmail(email, noticeField),
+    () => validateName(name, noticeField),
+    () => validatePassword(password, cPassword, noticeField),
+    () => validateLegalAcceptance(noticeField),
+  ];
 
-  isValid = validateEmail(email, noticeField);
-
-  isValid = validatePassword(password, cPassword, noticeField);
-  
-  if (!acceptedLegal) {
-    const checkButton = document.getElementById("signup_check_off");
-    console.log("Please accept the Legal notice.");
-    noticeField.innerHTML += `<div>Please accept the Legal notice.</div>`;
-    checkButton.classList.add('bg-alert');
-    isValid = false;
-  }
+  const results = validations.map((validation) => validation());
+  const isValid = results.every((result) => result === true);
 
   if (!isValid) {
     throw new Error("Error in validation");
@@ -185,7 +238,25 @@ function validateInputs(email, password, cPassword) {
   return true;
 }
 
-async function getNewUserId() {
+function getUserInitials(name) {
+  const words = name.trim().split(/\s+/);
+
+  if (words.length === 1) {
+    return words[0].charAt(0).toUpperCase();
+  }
+
+  if (words.length === 2) {
+    return (words[0].charAt(0) + words[1].charAt(0)).toUpperCase();
+  }
+
+  if (words.length >= 3) {
+    return (
+      words[0].charAt(0) + words[words.length - 1].charAt(0)
+    ).toUpperCase();
+  }
+}
+
+async function getNewId() {
   let response = await fetch(`${BASE_URL_S}users/.json`);
   let responseToJson = await response.json();
   let newUserId;
@@ -205,13 +276,14 @@ function handleRegistrationResult(result) {
   }
 }
 
-async function addUser(email, name, password) {
+async function addUser(email, name, password, initials) {
   const userId = await getNewId();
   //Initialen ermitteln und übergeben
 
   try {
     const result = await postData(`${BASE_URL_S}users/${userId - 1}/`, {
       name: name,
+      initials: initials,
       email: email,
       password: password,
       id: userId,
@@ -242,36 +314,27 @@ async function postData(path = "", data = {}) {
 function handleRegistrationResult(result) {
   if (result) {
     console.log("Registration successful!");
-    resetForm();
-    // ToDos an dieser Stelle: Felder zurück setzen, Seite neu laden
   } else {
     console.log("There was a problem with the registration. Please try again.");
   }
 }
 
-function resetForm() {
+function resetFormInputs() {
   document.getElementById("signup_email").value = "";
-  document.getElementById("signup_email").classList.remove('border-alert');
-
   document.getElementById("signup_name").value = "";
-
   document.getElementById("signup_password").value = "";
-  document.getElementById("signup_password").classList.remove('border-alert');
-
   document.getElementById("signup_c_password").value = "";
-  document.getElementById("signup_c_password").classList.remove('border-alert');
 
-  document.getElementById(signup_check_off).src = `/assets/img/png/check-button-false.png`;
+  document.getElementById(
+    "signup_check_off"
+  ).src = `/assets/img/png/check-button-false.png`;
 }
 
-
-// 4.  zu login-----------------------------------------------------------
-
-
-
-// HAUPTFUNKTION----------------------------------------------------------
-// HAUPTFUNKTION----------------------------------------------------------
-// HAUPTFUNKTION----------------------------------------------------------
+function resetFormBorders() {
+  document.getElementById("signup_email").classList.remove("border-alert");
+  document.getElementById("signup_password").classList.remove("border-alert");
+  document.getElementById("signup_c_password").classList.remove("border-alert");
+}
 
 async function signUpProcess() {
   const email = document.getElementById("signup_email").value.trim();
@@ -279,20 +342,13 @@ async function signUpProcess() {
   const password = document.getElementById("signup_password").value;
   const cPassword = document.getElementById("signup_c_password").value;
 
-  // 1.  Form validieren
-  validateInputs(email, name, password, cPassword)
+  resetFormBorders();
+  validateInputs(email, name, password, cPassword);
 
-  // 2.  pushen der User
-  addUser(email, name, password);
+  const initials = getUserInitials(name);
 
-  // 3.  Form reset
-  resetForm();
-
-  // 4.  zu login
+  addUser(email, name, password, initials);
+  resetFormInputs();
+  resetFormBorders();
+  toggleAccessWindow();
 }
-
-
-
-
-
-
