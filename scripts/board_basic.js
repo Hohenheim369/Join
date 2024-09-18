ac5976730f398a5fe3c47da7b969f0daf469ace3
Susanne Renken <renken.susanne@gmail.com>
2024-09-19 00:05:49 +0200

    change svg to png

--- scripts/board_basic.js
+++ scripts/board_basic.js
@@ -12,3 +12,26 @@ function toggleOverlay() {
 function bubblingPrevention(event) {
   event.stopPropagation();
 }
+
+async function renderTasksInBoard() {
+  const users = await fetchData('users');
+  console.log(users);
+  
+}
+
+
+
+
+// function render() {
+//   for (let i = 0; i < dishes.length; i++) {
+//     const dish = dishes[i];
+
+//     renderPopulars(i, dish.popular, dish.name, dish.description, dish.price);
+//     renderMeal(i, dish.category, dish.name, dish.description, dish.price);
+//   }
+// }
+
+// function renderMeal(i, category, name, description, price) {
+//   let meal = document.getElementById(category);
+//   meal.innerHTML += generateDishHtml(i, name, description, price);
+// }
