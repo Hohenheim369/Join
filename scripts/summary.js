5b8085e23020f52258d85c512443f273601c3f55
alexej <alexej.kaljuzhin@googlemail.com>
2024-09-19 10:20:49 -0400

    summary count tasks summary Js Done

--- scripts/summary.js
+++ scripts/summary.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   greeting();
+  displayTasks();
 });
 
 function greeting() {
@@ -32,4 +33,48 @@ function getGreetingMessage() {
   }
 }
 
+async function displayTasks() {
+    tasks = await fetchData('tasks'); 
+    countToDo(tasks);
+    countDone(tasks);
+    countUrgent(tasks);
+    countTaskInBoard(tasks);
+    countTaskInProgress(tasks);
+    countTaskInFeedback(tasks);
+}
 
+function countToDo(tasks) {
+    let toDo = document.getElementById('count_to_do');
+    let count = tasks.filter(task => task.status === "todo").length;
+    toDo.innerHTML = `${count}`;
+}
+
+function countDone(tasks) {
+    let done = document.getElementById('count_done');
+    let count = tasks.filter(task => task.status === "done").length;
+    done.innerHTML = `${count}`;
+}
+
+function countUrgent(tasks) {
+    let urgent = document.getElementById('count_priority_urgent');
+    let count = tasks.filter(task => task.priority === "urgent").length;
+    urgent.innerHTML = `${count}`;
+}
+
+function countTaskInBoard(tasks) {
+    let taskInBoard = document.getElementById('count_tasks');
+    let count = tasks.length;
+    taskInBoard.innerHTML = `${count}`;
+}
+
+function countTaskInProgress(tasks) {
+    let taskInProgress = document.getElementById('count_progress');
+    let count = tasks.filter(task => task.status === "inprogress").length;
+    taskInProgress.innerHTML = `${count}`;
+}
+
+function countTaskInFeedback(tasks) {
+    let taskInFeedback = document.getElementById('count_feedback');
+    let count = tasks.filter(task => task.status === "feedback").length;
+    taskInFeedback.innerHTML = `${count}`;
+}
