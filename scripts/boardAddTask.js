661b4535902512c26ffcf66a0833e6f3cef5b2b2
LarsMadproduction <schumacher90lars@gmail.com>
2024-09-26 10:35:11 +0200

    sort function location

--- scripts/boardAddTask.js
+++ scripts/boardAddTask.js
@@ -10,3 +10,18 @@ function openEditDialog(taskId){
   overlay.classList.remove("d-none");  
   
 }
+
+function taskValuesToEditField(singleTask, contacts, id){
+  document.getElementById("title_input").value = singleTask.title;
+  document.getElementById("description_textarea").value = singleTask.description;
+  document.getElementById("due_date").value = singleTask.date;
+  // const existingUserIndex = userId.map(Number);
+  // removeUserAssigned(existingUserIndex);
+  // selectedContacts.length = 0;
+  // updateSelectedContactsDisplay();
+  // getContacts();
+  // selectPrio("medium");
+  document.getElementById("category").innerText = singleTask.category;
+  // subTasks.length = 0;
+  // document.getElementById("subtasks_list").innerHTML = "";
+}
