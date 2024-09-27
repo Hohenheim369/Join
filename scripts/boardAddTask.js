a04b67fca8221877b1973424046fd6ac49949a35
LarsMadproduction <schumacher90lars@gmail.com>
2024-09-27 11:57:54 +0200

    clean code update

--- scripts/boardAddTask.js
+++ scripts/boardAddTask.js
@@ -15,44 +15,6 @@ function openEditDialog(taskId) {
   editTaskButton.innerHTML = editTaskTemplate(taskId);
 }
 
-// function taskValuesToEditField(singleTask, contacts) {
-//   document.getElementById("title_input").value = singleTask.title;
-//   document.getElementById("description_textarea").value =
-//     singleTask.description;
-//   document.getElementById("due_date").value = singleTask.date;
-//   if (singleTask.user === activeUser.id) {
-//     addUserToTask("contact_to_task_0", "task", "bg_task_0", `${activeUser.id}`);
-//   }
-//   let userContacts = singleTask.assigned.filter((data) => data !== null);
-//   let contactsToRender = contacts.filter((contact) =>
-//     userContacts.includes(contact.id)
-//   );
-//   window.allContacts = contactsToRender;
-//   contactsToRender.forEach((contact) => {
-//     addContactToTask(
-//       `contact_to_task_${contact.id}`,
-//       "task",
-//       `bg_task_${contact.id}`,
-//       `${contact.id}`
-//     );
-//   });
-//   selectPrio(singleTask.priority);
-//   document.getElementById("category").innerText = singleTask.category;
-//   document.getElementById("edit_category").classList.add("d-none");
-//   document.getElementById("edit_category").classList.remove("form-child-order");
-//   const subtaskEdit = singleTask.subtasks;
-//   if (subtaskEdit) {
-//     subtaskEdit.forEach((subtask) => {
-//       subTasks.push(subtask.subTaskName);
-//       let ids = subTasks.length;
-//       document.getElementById("subtasks_list").innerHTML += addSubtasksToList(
-//         subtask.subTaskName,
-//         ids - 1
-//       );
-//     });
-//   }
-// }
-
 function taskValuesToEditField(singleTask, contacts) {
   setTaskBasicValues(singleTask);
   setTaskUser(singleTask);
@@ -118,27 +80,6 @@ function setTaskSubtasks(singleTask) {
   }
 }
 
-
-// function enableEditButton(taskId) {
-//   let input = document.getElementById("title_input");
-//   let date = document.getElementById("due_date");
-//   let category = document.getElementById("category").innerText;
-//   let createButton = document.getElementById("create_button");
-//   if (
-//     input.value.trim() !== "" &&
-//     date.value.trim() !== "" &&
-//     (category === "Technical Task" || category === "User Story")
-//   ) {
-//     createButton.disabled = false;
-//     resetrequiredFields();
-//     editTask(taskId);
-//   } else {
-//     createButton.disabled = true;
-//     requiredFields();
-//     createButton.disabled = false;
-//   }
-// }
-
 function enableEditButton(taskId) {
   const input = getInputFields();
   const category = getCategory();
@@ -177,35 +118,6 @@ function handleInvalidForm() {
   requiredFields();
 }
 
-
-// async function editTask(taskId) {
-//   let title = document.getElementById("title_input").value;
-//   let description = document.getElementById("description_textarea").value;
-//   let dueDate = document.getElementById("due_date").value;
-//   let categorySeleced = document.getElementById("category").innerText;
-//   let assignedTo = selectedContacts.map(Number);
-//   let tasks = await fetchData("tasks");
-//   let singleTask = tasks.find((task) => task.id === taskId);
-//   let currenttaskStatus = singleTask.status;
-//   let userTaskId = usertest(singleTask);
-//   putEditTasksContent(
-//     title,
-//     description,
-//     dueDate,
-//     taskId,
-//     assignedTo,
-//     categorySeleced,
-//     userTaskId,
-//     currenttaskStatus
-//   );
-//   // putTaskToUser(taskId);
-//   openAddTaskDialogFeedback();
-//   await sleep(1500);
-//   toggleOverlay("edit_task_overlay");
-//   toggleOverlay("board_task_overlay");
-//   closeAddTaskDialogFeedback();
-//   openSingleTask(taskId);
-// }
 
 async function editTask(taskId) {
   const taskData = getTaskFormData();
