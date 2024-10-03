db810a64bb3bb125b29a3bb1a7c455a8993506af
Susanne Renken <renken.susanne@gmail.com>
2024-10-03 18:41:27 +0200

    JS Docs

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ const BASE_URL =
   "https://join-b72fb-default-rtdb.europe-west1.firebasedatabase.app/";
 let activeUser = getActiveUser();
 
+/**
+ * Downloads the data from the database depending on the deposit and ID.
+ * 
+ * @param {string} path - The path in the database from where to load the data.
+ * @returns {Array} - Downloaded data.
+ */
 async function fetchData(path = "") {
   const response = await fetch(`${BASE_URL}/${path}/.json`);
   const datas = await response.json();
@@ -12,6 +18,13 @@ async function fetchData(path = "") {
   return dataArray.filter(data => data !== null);
 }
 
+/**
+ * Uploads the data from the database depending on the deposit and ID.
+ * 
+ * @param {string} path - The path in the database for which a new ID should be generated.
+ * @param {*} data - 
+ * @returns 
+ */
 async function postData(path = "", data = {}) {
   const response = await fetch(`${BASE_URL}/${path}/.json`, {
     method: "PUT",
@@ -27,6 +40,12 @@ async function postData(path = "", data = {}) {
   return await response.json();
 }
 
+/**
+ * Deletes the data from the database depending on the deposit and the ID.
+ * 
+ * @param {string} path - The path in the database for which a new ID should be generated.
+ * @param {*} id - Id of the element to be deleted
+ */
 async function deleteData(path = "", id) {
   const url = `${BASE_URL}/${path}/${id - 1}.json`;
   const response = await fetch(url, {
@@ -38,6 +57,12 @@ async function deleteData(path = "", id) {
   return await response.json();
 }
 
+/**
+ * Checks whether data exists in the database and returns the new ID.
+ * 
+ * @param {string} path - The path in the database for which a new ID should be generated.
+ * @returns {Number} - New ID
+ */
 async function getNewId(path = "") {
   let response = await fetch(`${BASE_URL}/${path}/.json`);
   let responseToJson = await response.json();
@@ -50,6 +75,12 @@ async function getNewId(path = "") {
   return newUserId;
 }
 
+/**
+ * Returns the new ID depending on the existing data in the database.
+ * 
+ * @param {number} responseToJson 
+ * @returns {Number} - New ID
+ */
 function countId(responseToJson) {
   let keys = Object.keys(responseToJson);
   let lastKey = keys[keys.length - 1];
@@ -58,6 +89,10 @@ function countId(responseToJson) {
   return countId;
 }
 
+/**
+ * Loads the user's data for the activeUser into the LocalStorage
+ * @returns {Objekt} - ActiveUser data 
+ */
 function getActiveUser() {
   try {
     const storedUser = localStorage.getItem("activeUser");
@@ -72,6 +107,9 @@ function getActiveUser() {
   }
 }
 
+/**
+ * Resets the default tasks and contacts in the database.
+ */
 async function resetTheDatabase() {
   for (let index = 0; index < dbBackupTask.length; index++) {
       await postData(`tasks/${index}/`, dbBackupTask[index]);
@@ -79,6 +117,12 @@ async function resetTheDatabase() {
   }  
 }
 
+/**
+ * Changes the image of the check button.
+ * 
+ * @param {number} CheckButtonId Id of the check-button
+ * @param {HTMLElement} CheckTaskButton The HTML element where the button is displayed
+ */
 function toggleCheckButton(CheckButtonId, CheckTaskButton) {
   const checkButton = document.getElementById(CheckButtonId);
   const isChecked = checkButton.src.includes("true");
@@ -109,6 +153,9 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 });
 
+/**
+ * Prevents the function from executing on the Parend element.
+ */
 function bubblingPrevention(event) {
   event.stopPropagation();
 }
